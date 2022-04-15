befac7d2bd5b292d4819cf1dee2f7abb2c92bdb0
Aylin Osmanova <aayylliinn2000@abv.bg>
2022-04-15 13:16:54 +0200

    Added new route in main.py

--- app/main.py
+++ app/main.py
@@ -7,3 +7,9 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Eventor": "Welcome to EVENTS service"}
+
+
+# Get all events
+@app.get("/events")
+def read_root():
+    return {"List of all events"}
