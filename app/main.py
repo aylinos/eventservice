e74b3c2da77804c07066c49a5cfd6df6ee9b71db
Aylin Osmanova <aayylliinn2000@abv.bg>
2022-05-09 00:59:34 +0200

    Simple publish / subscribe with RabbitMQ

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
+import pika
 from fastapi import FastAPI
 from mangum import Mangum  # Amazon Lambda handler
 
 from .routers import eventrouter, typerouter
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+
+channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
 
 app = FastAPI()
 
